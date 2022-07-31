778b0b317e063cfd1b1edd964e8450c797552c12
wiggleji <roamgom@gmail.com>
2022-07-31 12:59:38 +0900

    init mail repository

---
+++ src/test/java/com/example/mailService/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mailService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
