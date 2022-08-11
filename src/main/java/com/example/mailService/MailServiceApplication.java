65655e97568211e5c35b6e9e2a5a6f01ba777d56
wiggleji <roamgom@gmail.com>
2022-08-12 00:53:36 +0900

    edit initUsers to add unique admin on application PostConstruct

--- src/main/java/com/example/mailService/MailServiceApplication.java
+++ src/main/java/com/example/mailService/MailServiceApplication.java
@@ -12,25 +12,28 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class MailServiceApplication {
 
-	private final UserRepository repository;
+	private final UserRepository userRepository;
 
 	private final PasswordEncoder passwordEncoder;
 
-	public MailServiceApplication(UserRepository repository, PasswordEncoder passwordEncoder) {
-		this.repository = repository;
+	public MailServiceApplication(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
 		this.passwordEncoder = passwordEncoder;
 	}
 
 	@PostConstruct
 	public void initUsers() {
-		User user = User.builder()
-				.username("admin")
-				.email("admin@social.com")
-				.password(passwordEncoder.encode("test1234"))
-				.role(UserRole.ROLE_ADMIN)
-				.build();
+		userRepository.findByUsername("admin").orElseGet(() -> {
+			User user = User.builder()
+					.username("admin")
+					.email("admin@social.com")
+					.password(passwordEncoder.encode("test1234"))
+					.role(UserRole.ROLE_ADMIN)
+					.build();
 
-		repository.save(user);
+			userRepository.save(user);
+			return user;
+		});
 	}
 
 	public static void main(String[] args) {
