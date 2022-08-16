8382c9cba75007f7cad823e15b86c0468fbb5c2f
wiggleji <roamgom@gmail.com>
2022-08-16 13:18:15 +0900

    replace AuthService logic to inject from UserService

--- src/main/java/com/example/mailService/service/UserService.java
+++ src/main/java/com/example/mailService/service/UserService.java
@@ -1,10 +1,14 @@
 package com.example.mailService.service;
 
+import com.example.mailService.domain.dto.UserSignUpDto;
 import com.example.mailService.domain.entity.User;
 import com.example.mailService.repository.UserRepository;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -21,6 +25,11 @@ public class UserService {
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
     }
 
+    public boolean checkUserExistsByEmail(String email) {
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        return optionalUser.isPresent();
+    }
+
     public User loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
@@ -29,5 +38,17 @@ public class UserService {
     public User loadUserById(Long id) throws UsernameNotFoundException {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with id: " + id));
+    }
+
+    public User createUser(UserSignUpDto signUpDto) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        User newUser = User.builder()
+                .username(signUpDto.getUsername())
+                .email(signUpDto.getEmail())
+                .password(passwordEncoder.encode(signUpDto.getPassword()))
+                .build();
+
+        return userRepository.save(newUser);
     }
 }
